6d14215127c0465a7c0d41a1ac7925113135f03b
xukeze <xu.keze@intellif.com>
2018-10-09 11:29:42 +0800

    task3

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib import auth
 
 # Create your views here.
 
@@ -9,6 +9,21 @@ def index(request):
 
 
 def login_action(request):
-    if request.method == "GET":
-        print(request.GET.get("username"))
-    # return render(request, "index.html")
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        if username == "" or password == "":
+            return render(request, "index.html", {"error": "用户名或密码不能为空"})
+        else:
+            user = auth.authenticate(
+                username=username, password=password)
+            print(user)
+            print(type(user))
+
+            if user is not None:
+                auth.login(request, user)
+                return render(request, "project_manage.html")
+            else:
+                return render(request, "index.html", {"error": "用户名或密码错误"})
+
