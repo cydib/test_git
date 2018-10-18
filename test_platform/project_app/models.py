f01bb5e39e4a6f06f4db3209a47aa0a0f142cd59
xukeze <xu.keze@intellif.com>
2018-10-18 11:26:51 +0800

    修正project_app中的models,创建项目完成

--- test_platform/project_app/models.py
+++ test_platform/project_app/models.py
@@ -10,8 +10,8 @@ class Project(models.Model):
     '''
     name = models.CharField("名称", max_length=100, blank=False, default="")
     describe = models.TextField("描述", default="")
-    status = models.BooleanField("状态", default="")
-    create_time = models.DateTimeField("创建时间", auto_created=True)
+    status = models.BooleanField("状态", default=True)
+    create_time = models.DateTimeField("创建时间", auto_now=True)
 
     # 在admin后台显示创建时的标题，而不是project1、2
     def __str__(self):
