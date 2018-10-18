6bc39fcd515b7ab2fac518f1ef5a9cce20f7dd5b
xukeze <xu.keze@intellif.com>
2018-10-18 11:15:16 +0800

    检查错误

--- test_platform/project_app/urls.py
+++ test_platform/project_app/urls.py
@@ -3,7 +3,6 @@ from project_app import views
 
 urlpatterns = [
     path('project_manage/', views.project_manage),
-    path('project_manage/create_project/', views.create_project),
-    path('project_manage/create_project/save_project/', views.save_project),
+    path('add_project/', views.add_project),
 
 ]
--- test_platform/project_app/views.py
+++ test_platform/project_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from project_app.models import Project
+from .forms import ProjectForm
 
 
 # Create your views here.
@@ -14,10 +15,17 @@ def project_manage(request):
 
 
 @login_required
-def create_project(request):
-    return render(request, "project_manage.html", {"type": "add"})
+def add_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+            print("name", name)
+            print("describe", describe)
+            Project.objects.create(name=name, describe=describe)
+            return HttpResponseRedirect('/manage/project_manage/')
+    else:
+        form = ProjectForm()
+    return render(request, "project_manage.html", {'form': form, "type": "add"})
 
-
-@login_required
-def save_project(request):
-    return render(request, "project_manage.html", {"type": "save"})
