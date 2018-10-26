d1690e2ee1ff8d4009757601a11286e03c3adf60
cydib <382643558@qq.com>
2018-10-26 22:01:53 +0800

    update testUI

--- test_platform/user_app/tests/testUI.py
+++ test_platform/user_app/tests/testUI.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-04-02 22:34:42
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
 from django.test import TestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver import Chrome
@@ -18,12 +25,12 @@ class LoginTests(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_login_null(self):
-        self.selenium.get('%s%s' %(self.live_server_url, "/"))
+        self.selenium.get('%s%s' % (self.live_server_url, "/"))
         username = self.selenium.find_element_by_name("username")
         username.send_keys("")
         password = self.selenium.find_element_by_name("password")
         password.send_keys("")
-        self.selenium.find_element_by_id("loginButton").click()
+        self.selenium.find_element_by_xpath("/html/body/div/form/button").click()
         error_text = self.selenium.find_element_by_id("error").text
         sleep(2)
         self.assertEquals("用户名或密码不能为空", error_text)
