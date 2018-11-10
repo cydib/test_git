b177b5a024754a8c658f882d6ee1efe5e65bb614
cydib <382643558@qq.com>
2018-11-10 21:23:43 +0800

    创建用例表失败，不知道什么原因

--- test_platform/interface_app/models.py
+++ test_platform/interface_app/models.py
@@ -7,13 +7,15 @@ class TestCase(models.Model):
     '''
     用例表
     '''
-    moudle = models.ForeignKey(Module, on_delete=models.CASCADE, verbose_name='所属项目')
+
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
     name = models.CharField("用例名称", max_length=100, blank=False, default="")
     url = models.TextField("请求url", default="")
-    req_method = models.TextField("请求方法", default="")
-    par_type = models.TextField("参数类型", default="")
+    req_method = models.CharField("请求方法", max_length=10, default="")
+    par_type = models.CharField("参数类型", max_length=10, default="")
     req_headers = models.TextField("header", default="")
     req_parameter = models.TextField("参数", default="")
+    req_assert = models.TextField("验证", default="")
     create_time = models.DateTimeField("创建时间", auto_now=True)
 
     # 在admin后台显示创建时的标题，而不是project1、2
