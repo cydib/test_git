1e01a7011c7fa6803d24453d2fd445f82d90b48b
xukeze <xu.keze@intellif.com>
2018-11-12 16:33:58 +0800

    增加项目模块二级联动、保存测试用例功能

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('case_manage/', views.case_manage),
     path('debug/', views.debug),
     path('api_debug/', views.api_debug),
+    path('get_porject_list/', views.get_porject_list),
     path('save_case/', views.save_case),
-
 
 ]
--- test_platform/interface_app/views.py
+++ test_platform/interface_app/views.py
@@ -1,14 +1,36 @@
 import requests, json
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from project_app.models import Project, Module
+from interface_app.models import TestCase
 
 # Create your views here.
 
 
+# 获取项目模块列表
+def get_porject_list(request):
+    project_list = Project.objects.all()
+    dataList = []
+    for project in project_list:
+        project_dict = {
+            "name": project.name
+        }
+        module_list = Module.objects.filter(project_id=project.id)
+        if len(module_list) != 0:
+            module_name = []
+            for module in module_list:
+                module_name.append(module.name)
+
+            project_dict["moduleList"] = module_name
+            dataList.append(project_dict)
+
+    return JsonResponse({"success": "true", "data": dataList})
+
+
 def case_manage(request):
     if request.method == "GET":
-        return render(request, "case_manage.html", {"type": "list"})
+        testcase = TestCase.objects.all()
+        return render(request, "case_manage.html", {"type": "list","testcases": testcase})
     else:
         return HttpResponse("404")
 
@@ -39,7 +61,36 @@ def api_debug(request):
         return HttpResponse(r.text)
 
 
-
 def save_case(request):
+    """
+    保存测试用例
+    """
     if request.method == "POST":
-        return HttpResponse("Hello")
+        name = request.POST.get("name", "")
+        url = request.POST.get("req_url", "")
+        method = request.POST.get("req_method", "")
+        parameter = request.POST.get("req_parameter", "")
+        req_type = request.POST.get("req_type", "")
+        header = request.POST.get("header", "")
+        module_name = request.POST.get("module", "")
+
+        if url == "" or method == "" or req_type == "" or module_name == "":
+            return HttpResponse("必传参数为空")
+
+        if parameter == "":
+            parameter = "{}"
+
+        if header == "":
+            header = "{}"
+
+        module_obj = Module.objects.get(name=module_name)
+
+        case = TestCase.objects.create(name=name, module=module_obj, url=url,
+                                       req_method=method, req_headers=header,
+                                       par_type=req_type,
+                                       req_parameter=parameter)
+        if case is not None:
+            return HttpResponse("保存成功！")
+
+    else:
+        return HttpResponse("404")
