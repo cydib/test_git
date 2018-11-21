601114de499180e228ef906a227ddb3cd2c0c26f
xukeze <xu.keze@intellif.com>
2018-11-21 20:57:13 +0800

    mkdir interface_app views

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from interface_app import views
+from interface_app.views import testcase_views
 
 urlpatterns = [
-    path('case_manage/', views.case_manage),
-    path('add_case/', views.add_case),
-    path('debug_case/', views.debug_case),
-    path('edit_case/<int:cid>/', views.edit_case),
-    path('get_porject_list/', views.get_porject_list),
-    path('save_case/', views.save_case),
-    path('search_case_name/', views.search_case_name)
+    path('case_manage/', testcase_views.case_manage),
+    path('add_case/', testcase_views.add_case),
+    path('debug_case/', testcase_views.debug_case),
+    path('edit_case/<int:cid>/', testcase_views.edit_case),
+    path('get_porject_list/', testcase_views.get_porject_list),
+    path('save_case/', testcase_views.save_case),
+    path('search_case_name/', testcase_views.search_case_name)
 
 ]
